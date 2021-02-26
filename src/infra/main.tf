76af229a8338e7d376773cf554afacc9f26bee1c
heoelri <heoelri@microsoft.com>
2021-02-26 08:55:37 +0100

    update tf version

--- src/infra/main.tf
+++ src/infra/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "2.47.0"
+      version = "2.49.0"
     }
     random = {
       version = ">= 2.2.1"
