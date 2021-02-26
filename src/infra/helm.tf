17341094f92bcbc70090c419866d6394364df925
heoelri <heoelri@microsoft.com>
2021-02-26 08:58:52 +0100

    modify helm provider

--- src/infra/helm.tf
+++ src/infra/helm.tf
@@ -7,7 +7,6 @@ provider "helm" {
     client_certificate     = base64decode(azurerm_kubernetes_cluster.deployment.kube_config.0.client_certificate)
     client_key             = base64decode(azurerm_kubernetes_cluster.deployment.kube_config.0.client_key)
     cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.deployment.kube_config.0.cluster_ca_certificate)
-    load_config_file       = false
   }
 }
 
